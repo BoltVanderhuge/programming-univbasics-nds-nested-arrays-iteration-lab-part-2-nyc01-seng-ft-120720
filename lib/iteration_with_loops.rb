f7841065e73b87e2bc73ee6119bbe65e92e4a431
John Wisneski <john.j.wisneski@gmail.com>
2020-11-22 22:46:56 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ minimum_int = []
 row_index = 0
 while row_index < src.count do
   element_index = 0
-  longest_string_element = ""
+  longest_string_element = ()
   while element_index < src[row_index].count do
  
     
