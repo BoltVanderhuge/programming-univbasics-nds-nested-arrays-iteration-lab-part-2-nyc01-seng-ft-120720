23306390e956aa4aaca62a8a5d46ac7f4b4c1812
John Wisneski <john.j.wisneski@gmail.com>
2020-11-22 22:56:20 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,11 +5,11 @@ minimum_int = []
 row_index = 0
 while row_index < src.count do
   element_index = 0
-  lowest_int = ()
+  lowest_int = 0
   while element_index < src[row_index].count do
  
     
-    if src[row_index][element_index] > lowest_int
+    if src[row_index][element_index] < lowest_int
       lowest_int = src[row_index][element_index]
     end
     element_index += 1
